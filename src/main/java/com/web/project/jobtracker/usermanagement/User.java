57c0361fddc493b050ce2eb786b0daaafbc1cc44
bagaria <parthbagaria12@gmail.com>
2020-07-23 20:18:07 -0300

    profile endpoints added

---
+++ src/main/java/com/web/project/jobtracker/usermanagement/User.java
@@ -0,0 +1,61 @@
+package com.web.project.jobtracker.usermanagement;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String linkedInURL;
+    private String password;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLinkedInURL() {
+        return linkedInURL;
+    }
+
+    public void setLinkedInURL(String linkedInURL) {
+        this.linkedInURL = linkedInURL;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
